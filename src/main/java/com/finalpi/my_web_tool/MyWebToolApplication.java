9bf48a69e60b41ae0dc0eb5fe92e2cb8fb7310c6
wangzhiwen <704762157@qq.com>
2022-10-13 12:00:55 +0800

    项目初始化

---
+++ src/main/java/com/finalpi/my_web_tool/MyWebToolApplication.java
@@ -0,0 +1,13 @@
+package com.finalpi.my_web_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finalpi/my_web_tool/MyWebToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalpi.my_web_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
